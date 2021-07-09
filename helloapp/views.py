7cadecc67efa79c3c81b7e772c6bba243e5b0c5f
seojaeyong <wg7363@naver.com>
2021-07-09 12:40:21 +0900

    2020-07-09 거 뭐야 클래스 만들어서 text 반환 추가했음

--- helloapp/views.py
+++ helloapp/views.py
@@ -1,11 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from helloapp.models import NewModel
+
 
 def hello(request):
     if request.method == "POST":
+
+        temp = request.POST.get('input_text')
+
+        new_model = NewModel()
+        new_model.text = temp
+        new_model.save()
+
+
         return render(request, 'helloapp/helloworld.html',
-                      context={'text': 'POST METHOD!'})
+                      context={'new_model': new_model })
     else:
         return render(request, 'helloapp/helloworld.html',
                       context={'text': 'GET METHOD!'})
