7e643a1373921dff6733fd338cd99c95c517cd6c
seojaeyong <wg7363@naver.com>
2021-07-14 16:59:04 +0900

    2020-07-14 CreateView 활용 create탭 생성

--- helloapp/urls.py
+++ helloapp/urls.py
@@ -5,7 +5,7 @@ from helloapp.views import hello, AccountCreateView
 app_name = 'helloapp'
 
 urlpatterns = [
-    path('helloman/', hello, name = 'hello_man')
+    path('helloman/', hello, name = 'hello_man'),
 
-    path('create/',AccountCreateView.as_view(), name = 'create')
+    path('create/', AccountCreateView.as_view(), name = 'create')
 ]
