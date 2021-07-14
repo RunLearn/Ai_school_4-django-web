8da89593ad89f8fc0092e34ca2b5ac2d3548e73b
seojaeyong <wg7363@naver.com>
2021-07-14 17:41:33 +0900

    2020-07-14 login, logout 탭생성

--- helloapp/urls.py
+++ helloapp/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from helloapp.views import hello, AccountCreateView
@@ -6,6 +7,12 @@ app_name = 'helloapp'
 
 urlpatterns = [
     path('helloman/', hello, name = 'hello_man'),
+
+    path('login/', LoginView.as_view(template_name='helloapp/login.html'),
+         name='login'),
+
+    path('logout/', LogoutView.as_view(template_name='helloapp/logout.html'),
+         name='logout'),
 
     path('create/', AccountCreateView.as_view(), name = 'create')
 ]
--- project2/settings.py
+++ project2/settings.py
@@ -1,6 +1,8 @@
 import os
 from pathlib import Path
 
+from django.urls import reverse_lazy
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env_list = dict()
@@ -104,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
 TIME_ZONE = 'UTC'
 
@@ -128,3 +130,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = reverse_lazy('helloapp:hello_man')
+
+LOGOUT_REDIRECT_URL = reverse_lazy('helloapp:login')
