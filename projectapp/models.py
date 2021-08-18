a949bda9474381386d91bc2df975f2c5b88bac18
RunLearn <wg7363@naver.com>
2021-08-18 17:04:24 +0900

    8/18 commit 2

--- projectapp/models.py
+++ projectapp/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Project(models.Model):
     name = models.CharField(max_length=20, null=False)
     description = models.CharField(max_length=200, null=True, blank=True)
-    image = models.ImageField(upload_to='project/')
+    image = models.ImageField(upload_to='project/',null=False)
 
     create_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.name}'
