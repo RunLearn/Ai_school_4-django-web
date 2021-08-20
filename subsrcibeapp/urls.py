843ffc9aac241e1788debd974f81396c77cfda10
RunLearn <wg7363@naver.com>
2021-08-20 17:07:15 +0900

    8/20 commit 2

--- subsrcibeapp/urls.py
+++ subsrcibeapp/urls.py
@@ -1,3 +1,9 @@
-urlpatterns = [
+from django.urls import path
 
+from subsrcibeapp.views import SubscriptionView
+
+app_name = 'subscribeapp'
+
+urlpatterns = [
+    path('subcribe/<int:project_pk>', SubscriptionView.as_view(), name='subscribe')
 ]
--- subsrcibeapp/views.py
+++ subsrcibeapp/views.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.generic import RedirectView
+
+from projectapp.models import Project
+from subsrcibeapp.models import Subscription
+
+@method_decorator(login_required(login_url= reverse_lazy('helloapp:login')), 'get')
+class SubscriptionView(RedirectView):
+
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        project = Project.objects.get(pk=kwargs['project_pk'])
+
+        subscription = Subscription.objects.filter(user=user,
+                                                   project=project)
+
+        if subscription.exists():
+            subscription.delete()
+        else:
+            new_sub = Subscription()
+            Subscription(user=user, project=project).save()
+
+        return super().get(request, *args, **kwargs)
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('projectapp:detail', kwargs={'pk':kwargs['project_pk']})
